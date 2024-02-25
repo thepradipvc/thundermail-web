04da1b414f680b3126d8c2d59d2eaa57a3d3caba
Pradip Chaudhary <chaudharypradip.n@gmail.com>
2024-02-25 13:15:59 +0530

    Changed code to use env variable for base URL instead of localhost

--- src/auth/google.ts
+++ src/auth/google.ts
@@ -2,7 +2,7 @@ import { env } from "@/env.mjs";
 import { Google } from "arctic";
 
 export const google = new Google(
-  env.NEXT_PUBLIC_GOOGLE_CLIENT_ID,
+  env.GOOGLE_CLIENT_ID,
   env.GOOGLE_CLIENT_SECRET,
-  "http://localhost:3000/api/auth/google/callback"
+  `${env.NEXT_PUBLIC_BASE_URL}/api/auth/google/callback`
 );
--- src/env.mjs
+++ src/env.mjs
@@ -4,13 +4,14 @@ import { z } from "zod";
 export const env = createEnv({
   server: {
     DATABASE_URL: z.string().url(),
+    GOOGLE_CLIENT_ID: z.string(),
     GOOGLE_CLIENT_SECRET: z.string(),
   },
   client: {
-    NEXT_PUBLIC_GOOGLE_CLIENT_ID: z.string(),
+    NEXT_PUBLIC_BASE_URL: z.string().url(),
   },
   // For Next.js >= 13.4.4, you only need to destructure client variables:
   experimental__runtimeEnv: {
-    NEXT_PUBLIC_GOOGLE_CLIENT_ID: process.env.NEXT_PUBLIC_GOOGLE_CLIENT_ID,
+    NEXT_PUBLIC_BASE_URL: process.env.NEXT_PUBLIC_BASE_URL,
   }
 });
