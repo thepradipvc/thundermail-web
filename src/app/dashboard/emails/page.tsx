7a09ae0b7d75face623cdd1c78b3f350ff2e38c0
Pradip Chaudhary <88336529+thepradipvc@users.noreply.github.com>
2024-05-04 12:34:17 +0530

    feat: Added search Filters and Pagination to emails dashboard page

--- src/app/dashboard/emails/page.tsx
+++ src/app/dashboard/emails/page.tsx
@@ -3,7 +3,8 @@ import Pagination from "@/components/Pagination";
 import { db } from "@/db";
 import { emailRecipients, emails } from "@/db/schema";
 import { serverClient } from "@/trpc/server-client";
-import { and, eq, gte } from "drizzle-orm";
+import { and, asc, countDistinct, desc, eq, gte, ilike, or } from "drizzle-orm";
+import { PgSelect } from "drizzle-orm/pg-core";
 import { redirect } from "next/navigation";
 import { z } from "zod";
 import EmailsTable from "./_EmailsTable";
@@ -28,15 +29,56 @@ const Page = async ({ searchParams }: { searchParams: SearchParams }) => {
   const apiKeyFilter = apiKeyFilterSchema.parse(searchParams.apiKey);
   const statusFilter = statusFilterSchema.parse(searchParams.status);
   const searchTerm = searchParams.search || "";
-
   const pageSize = 50;
-  const page =
-    parseInt(searchParams.page!) >= 1 ? parseInt(searchParams.page!) : 1;
-  const offset = (page - 1) * pageSize;
-  const totalPages = 1;
 
-  // TODO: Multiple where clauses not working
-  const query = db
+  const withFilters = <T extends PgSelect>(qb: T) => {
+    return qb.where(
+      and(
+        eq(emails.userId, user.id),
+        timeFilter ? gte(emails.createdAt, timeFilter) : undefined,
+        apiKeyFilter ? eq(emails.apiKeyId, apiKeyFilter) : undefined,
+        statusFilter ? eq(emailRecipients.status, statusFilter) : undefined,
+        or(
+          searchTerm ? ilike(emails.subject, `%${searchTerm}%`) : undefined,
+          searchTerm
+            ? ilike(emailRecipients.recepientEmail, `%${searchTerm}%`)
+            : undefined,
+        ),
+      ),
+    );
+  };
+
+  let totalResultsQuery = db
+    .select({ value: countDistinct(emails.id) })
+    .from(emails)
+    .innerJoin(emailRecipients, eq(emails.id, emailRecipients.emailId))
+    .$dynamic();
+
+  totalResultsQuery = withFilters(totalResultsQuery);
+  const totalResults = await totalResultsQuery.execute();
+  const totalPages = Math.ceil(totalResults[0].value / pageSize);
+
+  let page =
+    parseInt(searchParams.page!) >= 1 ? parseInt(searchParams.page!) : 1;
+  page = page > totalPages ? totalPages : page;
+  const offset = (page - 1) * pageSize;
+
+  const withPagination = <T extends PgSelect>(qb: T) => {
+    return qb
+      .orderBy(desc(emails.createdAt), asc(emails.id))
+      .limit(pageSize)
+      .offset(offset);
+  };
+
+  const emailIdsQuery = db
+    .selectDistinctOn([emails.createdAt, emails.id], { id: emails.id })
+    .from(emails)
+    .innerJoin(emailRecipients, eq(emails.id, emailRecipients.emailId))
+    .$dynamic();
+
+  const sq = withPagination(withFilters(emailIdsQuery)).as("subquery");
+
+  const filteredEmails = await db
     .select({
       id: emails.id,
       from: emails.from,
@@ -46,17 +88,10 @@ const Page = async ({ searchParams }: { searchParams: SearchParams }) => {
       to: emailRecipients.recepientEmail,
     })
     .from(emails)
+    .innerJoin(sq, eq(emails.id, sq.id))
     .innerJoin(emailRecipients, eq(emails.id, emailRecipients.emailId));
 
-  if (timeFilter) {
-    query.where(
-      and(eq(emails.userId, user.id), gte(emails.createdAt, timeFilter)),
-    );
-  } else {
-    query.where(eq(emails.userId, user.id));
-  }
-
-  const emailsData = refineData(await query.execute());
+  const emailsData = refineData(filteredEmails, searchTerm);
 
   const apiKeysData = await (await serverClient).apiKeys.getUserAPIKeys();
   const apiKeys = apiKeysData.map((apiKey) => ({
@@ -74,13 +109,13 @@ const Page = async ({ searchParams }: { searchParams: SearchParams }) => {
         <SearchAndFilter apiKeys={apiKeys} />
       </div>
       <EmailsTable emails={emailsData} />
-      {totalPages > 1 ? (
-        <div className="mx-auto">
-          <Pagination totalPages={totalPages} />
-        </div>
-      ) : (
-        <p className="text-sm text-muted-foreground">Page 1 of 1</p>
-      )}
+
+      <div className="mx-auto">
+        <Pagination totalPages={totalPages} />
+      </div>
+      <p className="ml-auto mt-0 text-sm text-muted-foreground">
+        Total Pages: {totalPages}
+      </p>
     </main>
   );
 };
@@ -98,6 +133,7 @@ type Email = {
 
 const refineData = (
   emails: Email[],
+  searchTerm: string,
 ): (Omit<Email, "to"> & { to: string[] })[] => {
   let map = new Map();
   for (const email of emails) {
@@ -111,7 +147,11 @@ const refineData = (
         to: [email.to],
       });
     } else {
-      map.get(email.id).to.push(email.to);
+      if (email.to.includes(searchTerm) || email.subject.includes(searchTerm)) {
+        map.get(email.id).to.unshift(email.to);
+      } else {
+        map.get(email.id).to.push(email.to);
+      }
     }
   }
 
