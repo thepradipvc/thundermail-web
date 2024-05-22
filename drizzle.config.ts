53c5c3adcd708f3cfc315b04d265d5ccdc2c7ce2
Pradip Chaudhary <88336529+thepradipvc@users.noreply.github.com>
2024-05-22 15:53:38 +0530

    chore: upgraded drizzle-kit to version 0.21.*

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -3,9 +3,9 @@ import { defineConfig } from "drizzle-kit";
 export default defineConfig({
   schema: "./src/db/schema.ts",
   out: "./src/db/migrations",
-  driver: "pg",
+  dialect: "postgresql",
   dbCredentials: {
-    connectionString: process.env.DATABASE_URL!,
+    url: process.env.DATABASE_URL!,
   },
   verbose: true,
   strict: true,
