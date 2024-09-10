68e90c28de169d6cc2ef92756d22b1266e8c6ddc
Pradip Chaudhary <88336529+thepradipvc@users.noreply.github.com>
2024-09-10 23:16:38 +0530

    updated pnpm version in github workflow and added run on dispatch to the workflow

--- src/lib/thundermail.ts
+++ src/lib/thundermail.ts
@@ -1,3 +1,6 @@
 import { ThunderMail } from "thundermail";
 
-export const thundermail = new ThunderMail(process.env.THUNDERMAIL_API_KEY);
+export const thundermail = new ThunderMail(
+  // Dummy API key for CI/CD
+  process.env.THUNDERMAIL_API_KEY || "tim_123",
+);
